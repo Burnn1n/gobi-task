cbfa75b5ba80838757d35d493dc5e99563ee4024
Erbold Burn <erbold_0514@yahoo.com>
2021-09-01 02:56:22 +0800

    section1_done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,7 @@ function Home({history}) {
   return (
 	<React.Fragment>
 		<div className='page-content'>
-			<div className="section_1 mx-0">
+			<div className="section_1_desktop mx-0">
 				<Row>
 					<Col sm="5" id="col1">
 						<div className="title">
@@ -31,6 +31,7 @@ function Home({history}) {
                 </div>
 					</Col>
 					<Col sm="7" id="col2">
+						<div>
 						<Row>
 							<div id="img1">
 								<img src={img1} alt="" />
@@ -39,13 +40,46 @@ function Home({history}) {
 								<div id="img2">
 								<img src={img2} alt="" />
 								</div>
-								<div>
+								<div id="img3">
 								<img src={img3} alt="" />
 								</div>
 							</div>
 						</Row>
+						</div>
+						
 					</Col>
 				</Row>
+			</div>
+			<div className="section_1_phone">
+				<div id="col1">
+							<div id="img1">
+								<img src={img1} alt="" />
+							</div>
+							<div>
+								<div id="img2">
+								<img src={img2} alt="" />
+								</div>
+								<div id="img3">
+								<img src={img3} alt="" />
+								</div>
+							</div>
+				</div>
+				<div id="col2">
+				<div className="title">
+							WINTER SPICE 2020
+						</div>
+						
+						<div className="text">
+							Infusion of summer pieces and cashmere silk blends.
+						</div>
+						<div className="shopNow">
+                <button>
+                  <span>
+                  Shop now
+                  </span>
+                  </button>
+                </div>
+				</div>
 			</div>
 
 			<div>first</div>
