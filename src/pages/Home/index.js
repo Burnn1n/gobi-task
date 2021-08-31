07a8f05b662eebaf38fcc36f470adf3942f79d32
Erbold Burn <erbold_0514@yahoo.com>
2021-09-01 04:48:22 +0800

    collection add

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,11 @@ import {
 import img1 from '../../assets/images/page/img1.png'
 import img2 from '../../assets/images/page/img2.png'
 import img3 from '../../assets/images/page/img3.png'
+import img4 from '../../assets/images/page/img4.png'
+import img5 from '../../assets/images/page/img5.png'
+import img6 from '../../assets/images/page/img6.png'
+import img7 from '../../assets/images/page/img7.png'
+import arrow from '../../assets/images/page/arrow.png'
 import './index.scss'
 function Home({history}) {
   return (
@@ -81,9 +86,68 @@ function Home({history}) {
                 </div>
 				</div>
 			</div>
+			<div className="section_2">
+				<div className="women-collec1">
+					<span className="collec-name">
+						CARDIGANS
+					</span>
+					<img src={arrow} className="arrow-link" alt="" />
+				<img src={img4}  alt="" />
+				</div>
+				<div className="women-collec2">
+				<span className="collec-name">
+						DRESSES
+					</span>
+					<img src={arrow} className="arrow-link" alt="" />
+					<span className="collec-col">
+						WOMEN
+					</span>
+					<br />
+				<img src={img5}  alt="" />
+				</div>
+				<div className="women-collec3">
+				<span className="collec-name">
+						CARDIGANS
+					</span>
+					<img src={arrow} className="arrow-link" alt="" />
+				<img src={img4}  alt="" />
+				</div>
+				
 
-			<div>first</div>
-			<div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>first</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div><div>hey</div>v
+			</div>
+			<div className="section_2">
+				<div className="women-collec1">
+				<span className="collec-name">
+						TURTLENECK
+					</span>
+					<img src={arrow} className="arrow-link" alt="" />
+					<img src={img6}  alt="" />
+				</div>
+				<div className="women-collec2">
+				<span className="collec-name">
+						POLO
+					</span>
+					<img src={arrow} className="arrow-link" alt="" />
+					<span className="collec-col">
+						MEMEN
+					</span>
+					<br />
+				<img src={img7}  alt="" />
+				</div>
+				<div className="women-collec3" >
+				<span className="collec-name">
+						TURTLENECK
+					</span>
+					<img src={arrow} className="arrow-link" alt="" />
+					<img src={img6}  alt="" />
+				</div>
+			</div>
+			<div className="section_3">
+				<span>TO GET AND TO GIFT</span>
+			</div>
+			<div className="section_4">
+				
+			</div>
 		</div>
 	</React.Fragment>
   );
