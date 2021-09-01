fef3f699788bcc23c3f04594a6da5b259a6e1501
Erbold Burn <erbold_0514@yahoo.com>
2021-09-01 13:13:18 +0800

    homa page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,6 +12,16 @@ import img5 from '../../assets/images/page/img5.png'
 import img6 from '../../assets/images/page/img6.png'
 import img7 from '../../assets/images/page/img7.png'
 import arrow from '../../assets/images/page/arrow.png'
+import img8 from '../../assets/images/page/img8.png'
+import img9 from '../../assets/images/page/img9.png'
+import img10 from '../../assets/images/page/img10.png'
+import img11 from '../../assets/images/page/img11.png'
+import img12 from '../../assets/images/page/img12.png'
+import img13 from '../../assets/images/page/img13.png'
+import img14 from '../../assets/images/page/img14.png'
+import img15 from '../../assets/images/page/img15.png'
+import arrow_phone from '../../assets/images/page/arrow-phone.png'
+
 import './index.scss'
 function Home({history}) {
   return (
@@ -92,7 +102,7 @@ function Home({history}) {
 						CARDIGANS
 					</span>
 					<img src={arrow} className="arrow-link" alt="" />
-				<img src={img4}  alt="" />
+				<img src={img4} className="collec-img"  alt="" />
 				</div>
 				<div className="women-collec2">
 				<span className="collec-name">
@@ -103,17 +113,59 @@ function Home({history}) {
 						WOMEN
 					</span>
 					<br />
-				<img src={img5}  alt="" />
+				<img src={img5} className="collec-img"  alt="" />
 				</div>
 				<div className="women-collec3">
 				<span className="collec-name">
 						CARDIGANS
 					</span>
 					<img src={arrow} className="arrow-link" alt="" />
-				<img src={img4}  alt="" />
+				<img src={img4} className="collec-img"  alt="" />
 				</div>
 				
 
+			</div>
+			<div className="section_2_mobile">
+				<div className="collec-col">
+					WOMEN
+				</div>
+				<div className="collec-content">
+				<div className="collec-div">
+				<span className="collec-name">
+						КАРДИГАНЫ
+					</span>
+					<img src={arrow_phone} className="arrow-link" alt="" />
+				<img src={img4} className="collec-img"   alt="" />
+				</div>
+				<div>
+				<span className="collec-name">
+						ПЛАТЬЯ
+					</span>
+					<img src={arrow_phone} className="arrow-link" alt="" />
+				<img src={img5} className="collec-img"  alt="" />
+				</div>
+				</div>
+			</div>
+			<div className="section_2_mobile">
+				<div className="collec-col">
+					MEN
+				</div>
+				<div className="collec-content">
+				<div className="collec-div">
+				<span className="collec-name">
+					захтай
+					</span>
+					<img src={arrow_phone} className="arrow-link" alt="" />
+				<img src={img6} className="collec-img"   alt="" />
+				</div>
+				<div>
+				<span className="collec-name">
+					ПОЛО
+					</span>
+					<img src={arrow_phone} className="arrow-link" alt="" />
+				<img src={img7} className="collec-img"  alt="" />
+				</div>
+				</div>
 			</div>
 			<div className="section_2">
 				<div className="women-collec1">
@@ -121,7 +173,7 @@ function Home({history}) {
 						TURTLENECK
 					</span>
 					<img src={arrow} className="arrow-link" alt="" />
-					<img src={img6}  alt="" />
+					<img src={img6} className="collec-img"  alt="" />
 				</div>
 				<div className="women-collec2">
 				<span className="collec-name">
@@ -132,21 +184,44 @@ function Home({history}) {
 						MEMEN
 					</span>
 					<br />
-				<img src={img7}  alt="" />
+				<img src={img7} className="collec-img"  alt="" />
 				</div>
 				<div className="women-collec3" >
 				<span className="collec-name">
 						TURTLENECK
 					</span>
 					<img src={arrow} className="arrow-link" alt="" />
-					<img src={img6}  alt="" />
+					<img src={img6} className="collec-img"  alt="" />
 				</div>
 			</div>
 			<div className="section_3">
 				<span>TO GET AND TO GIFT</span>
 			</div>
 			<div className="section_4">
-				
+				<div className="section_4_items">
+					<img src={img8} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img9} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img10} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img11} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img12} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img13} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img14} alt="" />
+				</div>
+				<div className="section_4_items">
+					<img src={img15} alt="" />
+				</div>
 			</div>
 		</div>
 	</React.Fragment>
